8af818b4551e1c1a570a13c84a857b93645d6957
GarrikUni <garrik@edu.univali.br>
2025-05-25 20:31:53 -0300

    Add files via upload

--- schedule_rr.h
+++ schedule_rr.h
@@ -4,10 +4,9 @@
 #include "task.h" // Defines the Task structure
 #include "list.h"   // Uses the list structure for the ready queue
 
-// Global ready queue for RR tasks
-// It's often better to pass the list to functions,
-// but the project hints at global list for simplicity [cite: 1]
 extern struct node *rr_ready_queue;
+
+void* timer_function(void* arg);
 
 // Add a task to the RR ready queue
 void rr_add(char *name, int priority, int burst);
